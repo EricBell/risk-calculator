05bee8550c59ac59cd1c52420b9dd4b47df54073
Eric Bell <eric@polymorph.co>
2025-12-28 12:22:42 -0500

    last update - don't remember

---
+++ main.py
@@ -0,0 +1,6 @@
+"""Entry point for Flet build - launches the Risk Calculator application."""
+from risk_calculator.main import run_app
+import sys
+
+if __name__ == "__main__":
+    sys.exit(run_app())
